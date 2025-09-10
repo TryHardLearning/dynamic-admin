fe7725e71c9b221184ef030a48d3312316db1014
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-09-10 17:22:14 -0300

    feat: add auth context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import AuthContextProvider from './context/AuthContext.jsx'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </StrictMode>
   </BrowserRouter>,
 )
