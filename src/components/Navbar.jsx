9edfef4379ae07785bcaf032292cc8af8418744d
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-10-24 15:56:08 -0300

    feat: logout function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+
+  const { logout } = useContext(AuthContext);
+
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img src={assets.logo} alt='Logo' className='w-[max(10%,80px)]'/>
-        <button className='bg-red-400 text-white px-5 py-2 sm:py-2 rounded-[8px] text-xs sm:text-sm'>Logout</button>
+      <img src={assets.logo} alt='Logo' className='w-[max(10%,80px)]' />
+      <button onClick={ () => logout() } className='bg-red-400 text-white px-5 py-2 sm:py-2 rounded-[8px] text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
